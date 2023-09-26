492cc457e85dfc5c65520c20c3dfd34e246773b5
sadiahossain1 <sadiah16@nycstudents.net>
2023-09-26 12:31:34 -0400

    created variables

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter the total bill: ");
+        double bill = scan.nextDouble();
+
+        System.out.print("Please enter the tip percentage: ");
+        int tip = scan.nextInt();
+
+    }
+}
