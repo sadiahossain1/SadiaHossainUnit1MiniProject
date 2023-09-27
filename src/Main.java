c767111163ca376c74bb1104c4a5788705b052eb
sadiahossain1 <sadiah16@nycstudents.net>
2023-09-27 12:07:04 -0400

    added all calculations and variables

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Please enter the total bill: ");
+        System.out.print("Please enter the total bill amount: ");
         double bill = scan.nextDouble();
 
         System.out.print("Please enter the tip percentage: ");
         int tip = scan.nextInt();
 
+        System.out.print("Please enter the number of people in the party: ");
+        int numberOfPeople = scan.nextInt();
+
+        double tipPerPerson = (bill * (tip/100.00)) / numberOfPeople;
+        System.out.println(tipPerPerson);
+
+        double totalBill = bill + (tipPerPerson * numberOfPeople);
+        System.out.println(totalBill);
+
+        double totalPerPerson = totalBill/numberOfPeople;
+        System.out.println(totalPerPerson);
+
+        double totalTipAmount = tipPerPerson * numberOfPeople;
+        System.out.println(totalTipAmount);
     }
 }
